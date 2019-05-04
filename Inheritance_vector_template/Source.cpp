6ab85039666a2de74796524838e13a7a0bc5f5a2
dinara0708 <aikenova01@gmail.com>
2019-05-04 14:11:41 +0600

    Добавьте файлы проекта.

---
+++ Inheritance_vector_template/Source.cpp
@@ -0,0 +1,31 @@
+﻿#include"extendedVector.h"
+
+int main() {
+
+	extendedVector<int> eV;
+
+	//777???
+
+	//for (int i = 0; i < 5; i++) {
+	//	eV.add_front(100);
+	//	eV.add(777);//why not first?
+	//	//eV.add_front(100);
+	//}
+
+	//eV.add_pos(5, 999);
+	//eV.print();
+	for (int i = 0; i < 5; i++){
+		eV.add_front(i+1);
+		eV.add(777);
+	}
+
+	//eV.del();
+	//eV.del_front();
+	//eV.del_pos(9);
+	eV.add_pos(7, 100000);
+	cout << endl;
+	eV.print();
+
+	system("pause");
+	return 0;
+}
---
+++ Inheritance_vector_template/extendedVector.h
@@ -0,0 +1,110 @@
+﻿#pragma once
+#include"Vector.h"
+
+template <typename T>
+class extendedVector :public Vector<T> {
+public:
+	extendedVector() :Vector<T>() {}
+	extendedVector(int size) :Vector<T>(size) {}
+	~extendedVector();
+
+	void del_front();
+	void add_front(T el);
+
+	void del_pos(int pos);
+	void add_pos(int pos, T el);
+
+	bool empty();
+};
+
+template<typename T>
+inline extendedVector<T>::~extendedVector()
+{
+	cout << "Destcruct extended vec" << endl;
+	delete[] Vector<T>::els;
+}
+
+template<typename T>
+inline void extendedVector<T>::del_front()
+{
+	for (int i = 0; i < Vector<T>::size(); i++)
+		Vector<T>::els[i] = Vector<T>::els[i + 1];
+	Vector<T>::cur_size--;
+}
+
+template<typename T>
+inline void extendedVector<T>::add_front(T el)
+{
+	if (Vector<T>::buf_size == 0)
+	{
+		Vector<T>::buf_size = 4;
+		Vector<T>::els = new T[Vector<T>::buf_size];
+		Vector<T>::els[0] = el;
+		Vector<T>::cur_size++;
+	}
+	else if (Vector<T>::cur_size == Vector<T>::buf_size) {
+		Vector<T>::buf_size *= 2;
+		T *tmp = new T[Vector<T>::buf_size];
+
+		tmp[0] = el;
+		for (int i = 0; i < Vector<T>::cur_size; i++)
+			tmp[i + 1] = Vector<T>::els[i];
+		delete[] Vector<T>::els;
+		Vector<T>::els = tmp;
+		Vector<T>::cur_size++;
+		//return;
+	}
+	else if (this->cur_size < this->buf_size) {
+		Vector<T>::els[0] = el;
+		for (int i = 0; i < Vector<T>::cur_size; i++)
+			Vector<T>::els[i + 1] = Vector<T>::els[i];
+
+		Vector<T>::cur_size++;
+	}
+}
+
+template<typename T>
+inline void extendedVector<T>::del_pos(int pos)
+{
+	for (int i = pos; i < Vector<T>::size(); i++)
+		Vector<T>::els[i] = Vector<T>::els[i + 1];
+	Vector<T>::cur_size--;
+}
+
+template<typename T>
+inline void extendedVector<T>::add_pos(int pos, T el)
+{
+	if (Vector<T>::buf_size == 0)
+	{
+		Vector<T>::buf_size = pos + 1;//minimal needed memory
+		Vector<T>::els = new T[Vector<T>::buf_size];
+		for (int i = 0; i < pos; i++)
+			Vector<T>::els[i] = 0;
+		Vector<T>::els[pos] = el;
+		Vector<T>::cur_size = pos + 1;
+	}
+	else if (Vector<T>::cur_size == Vector<T>::buf_size) {
+		Vector<T>::buf_size *= 2;
+		T *tmp = new T[Vector<T>::buf_size];
+
+		tmp[pos] = el;
+		for (int i = pos; i < Vector<T>::cur_size; i++)
+			tmp[i + 1] = Vector<T>::els[i];
+		delete[] Vector<T>::els;
+		Vector<T>::els = tmp;
+		Vector<T>::cur_size++;
+	}
+	else if (this->cur_size < this->buf_size) {
+		Vector<T>::els[pos] = el;
+		for (int i = pos; i < Vector<T>::cur_size; i++)
+			Vector<T>::els[i + 1] = Vector<T>::els[i];
+
+		Vector<T>::cur_size++;
+	}
+}
+
+template<typename T>
+inline bool extendedVector<T>::empty()
+{
+	return(Vector<T>::cur_size == 0);
+}
